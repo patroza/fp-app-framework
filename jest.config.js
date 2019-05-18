2ebe66c791761784d134ef1cc76fb9e1a0163d28
Patrick Roza <contact@patrickroza.com>
2019-05-18 19:16:30 +0200

    Fix ts config

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,6 @@ module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
-  "moduleNameMapper": {
-    "^@/(.*)$": "<rootDir>/src/$1"
-  },
   "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   "testURL": "http://localhost:8110",
   "moduleFileExtensions": [
