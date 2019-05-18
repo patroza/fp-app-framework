9f39b3956afc31f3c490e5b4c3f8814d66d870af
Patrick Roza <contact@patrickroza.com>
2019-05-18 16:10:50 +0200

    Improve flexibility of event handler. Improved logging and naming.

--- src/infrastructure/publishEvents.ts
+++ src/infrastructure/publishEvents.ts
@@ -3,11 +3,12 @@ import { publishEventsKey } from '../infrastructure/domainEventHandler'
 import { DomainEventReturnType, IntegrationEventReturnType } from '../infrastructure/misc'
 import { isTruthyFilter, logger } from '../utils'
 import { PipeFunction } from '../utils/neverthrow-extensions'
+import { UsecaseHandlerTuple } from './SimpleContainer'
 
-const publishEvents = (handlers: EventHandlerMap): typeof publishEventsKey => async events => {
+const publishEvents = (handlers: EventHandlerMap, resolve: CreateHandlerType): typeof publishEventsKey => async events => {
   const values: DomainEventReturnType[] = []
   for (const evt of events) {
-    const r = await processEvent(evt, handlers)
+    const r = await processEvent(evt, handlers, resolve)
     if (!r) { continue }
     if (r.isErr()) { return err(r) }
     r.value.forEach(x => values.push(x))
@@ -19,13 +20,19 @@ const publishEvents = (handlers: EventHandlerMap): typeof publishEventsKey => as
 export default publishEvents
 
 // TODO: Excellent usecase for a Mediator
-const processEvent = async (evt: any, handlers: EventHandlerMap): Promise<Result<IntegrationEventReturnType[], any>> => {
+const processEvent = async (
+  evt: any,
+  handlers: EventHandlerMap,
+  createHandler: CreateHandlerType,
+): Promise<Result<IntegrationEventReturnType[], any>> => {
   const hndl = handlers.get(evt.constructor)
   logger.log(`Publishing Domain event: ${evt.constructor.name} ${hndl ? hndl.length : 0} handlers ${JSON.stringify(evt)}`)
   const commitHandlers: IntegrationEventReturnType[] = []
   if (!hndl) { return ok(commitHandlers) }
-  for (const createHandler of hndl) {
-    const h = createHandler()
+  for (const evtHandler of hndl) {
+    // TODO: we can display the name of the handler here if we can resolve the handler here instead of pre-resolved.
+    const h = createHandler(evtHandler)
+    logger.log(`Handling ${evtHandler[3].name}`)
     const r = await h(evt)
     if (r.isErr()) { return err(r.error) }
     if (r.value) { commitHandlers.push(r.value) }
@@ -34,5 +41,5 @@ const processEvent = async (evt: any, handlers: EventHandlerMap): Promise<Result
   return ok(commitHandlers)
 }
 
-type EventHandlerMap = Map<any, EventHandlerFactory[]>
-export type EventHandlerFactory = () => PipeFunction<any, any, any>
+type EventHandlerMap = Map<any, Array<UsecaseHandlerTuple<any, any, any, any>>>
+export type CreateHandlerType = (hndlr: UsecaseHandlerTuple<any, any, any, any>) => PipeFunction<any, any, any>
