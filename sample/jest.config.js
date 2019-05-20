4536da7cc8d5c3feb987283f309872abafe74614
Patrick Roza <contact@patrickroza.com>
2019-05-20 09:45:15 +0200

    Fix test setup

--- sample/jest.config.js
+++ sample/jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   "moduleNameMapper": {
-    "^@/(.*)$": "<rootDir>/sample/src/$1",
-    "^fp-app-framework/(.*)": "<rootDir>/src/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^fp-app-framework/(.*)": "<rootDir>/../src/$1",
   },
   "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   "testURL": "http://localhost:8110",
