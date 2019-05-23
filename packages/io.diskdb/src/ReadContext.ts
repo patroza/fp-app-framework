3f76f06d615260105380174656d7867626b98fbb
Patrick Roza <contact@patrickroza.com>
2019-05-23 23:25:14 +0200

    Improve readContext

--- packages/io.diskdb/src/ReadContext.ts
+++ packages/io.diskdb/src/ReadContext.ts
@@ -3,29 +3,27 @@ import { err, ok, Result } from "@fp-app/neverthrow-extensions"
 import { getFilename } from "./RecordContext"
 import { deleteFile, exists, readFile, writeFile } from "./utils"
 
-const getReadFileName = (type: string, id: string) => getFilename(`read-${type}`, id)
-
 const deleteReadContextEntry = async (type: string, id: string) => {
   // Somehow this return an empty object, so we void it here.
-  await deleteFile(getReadFileName(type, id))
+  await deleteFile(getFilename(type, id))
 }
 
 const createOrUpdateReadContextEntry = <T>(type: string, id: string, value: T) =>
-  writeFile(getReadFileName(type, id), JSON.stringify(value))
+  writeFile(getFilename(type, id), JSON.stringify(value))
 
 const readReadContextEntry = async <T>(type: string, id: string) => {
-  const json = await readFile(getReadFileName(type, id), { encoding: "utf-8" })
+  const json = await readFile(getFilename(type, id), { encoding: "utf-8" })
   return JSON.parse(json) as T
 }
 
 export default class ReadContext<T> {
-  constructor(readonly type: string) { }
+  constructor(readonly type: string) { this.type = `read-${type}` }
 
   readonly create = (id: string, value: T) => createOrUpdateReadContextEntry(this.type, id, value)
   readonly delete = (id: string) => deleteReadContextEntry(this.type, id)
   readonly read = async (id: string): Promise<Result<T, RecordNotFound>> => {
     assert.isNotNull({ id })
-    const filePath = getFilename(`read-${this.type}`, id)
+    const filePath = getFilename(this.type, id)
     if (!await exists(filePath)) { return err(new RecordNotFound(this.type, id)) }
     const r = await readReadContextEntry<T>(this.type, id)
     return ok(r)
