28c3ef1f7db7908a9761e026c5a594ad99ec5037
Patrick Roza <contact@patrickroza.com>
2019-05-27 20:38:17 +0200

    Fix id.

--- samples/basic/src/TrainTrip/eventhandlers/integration.events.ts
+++ samples/basic/src/TrainTrip/eventhandlers/integration.events.ts
@@ -3,8 +3,8 @@ import { assert, Event } from "@fp-app/framework"
 // tslint:disable:max-classes-per-file
 
 export class CustomerRequestedChanges extends Event {
-  constructor(readonly trainTripId: string, readonly intineraryId: string) {
+  constructor(readonly trainTripId: string, readonly itineraryId: string) {
     super()
-    assert.isNotNull({ trainTripId, intineraryId })
+    assert.isNotNull({ trainTripId, itineraryId })
   }
 }
