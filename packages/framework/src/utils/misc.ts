4db1a11d2b9f2655d4c787180a04779127a20fc4
Patrick Roza <contact@patrickroza.com>
2019-06-06 11:17:46 +0200

    Freeze immutableObjs

--- packages/framework/src/utils/misc.ts
+++ packages/framework/src/utils/misc.ts
@@ -45,13 +45,15 @@ const createLazy = <T>(creatorFunction: () => T) => {
 
 // TODO: don't allow to put more properties in as default, than T
 function immutableObj<T>() {
-  return <TDefaults extends Partial<T>>(def?: TDefaults) =>
-    (current: Omit<T, keyof TDefaults> & Partial<T>, updates?: Partial<T>) => {
-      const newObj = { ...def, ...current, ...updates }
+  return <TDefaults extends Partial<T>>(def?: TDefaults) => {
+    const frozenDefault = Object.freeze({ ...def })
+    return (current: Omit<T, keyof TDefaults> & Partial<T>, updates?: Partial<T>) => {
+      const newObj = Object.freeze({ ...frozenDefault, ...current, ...updates })
       // TODO: consider
       // type UpdateableT = T & { update: (current: T, updates?: Partial<T>) => UpdateableT }
       return newObj as T
     }
+  }
 }
 
 export {
